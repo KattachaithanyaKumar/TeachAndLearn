005c8c655780f426597214d20eca639c4a15a220
kattachaithanyakumar <k.chaithanya278@gmail.com>
2025-06-19 10:47:18 +0530

    added the statistics and about us section

--- src/CONSTANTS.js
+++ src/CONSTANTS.js
@@ -1,3 +1,8 @@
+import { SlBadge } from "react-icons/sl";
+import { GoPeople } from "react-icons/go";
+import { FaRegHeart } from "react-icons/fa";
+import { MdOutlineMedicalServices } from "react-icons/md";
+
 export const navItems = [
   {
     label: "Home",
@@ -28,3 +33,41 @@ export const navItems = [
     path: "/contact-us",
   },
 ];
+
+export const statistics = [
+  {
+    label: "5+ Years Experience",
+    number: "5+",
+    icon: SlBadge,
+    iconColor: "text-amber-600",
+    bgColor: "bg-amber-100",
+  },
+  {
+    label: "30+ Therapists",
+    number: "30+",
+    icon: GoPeople,
+    iconColor: "text-teal-600",
+    bgColor: "bg-teal-100",
+  },
+  {
+    label: "150+ Happy Children",
+    number: "150+",
+    icon: FaRegHeart,
+    iconColor: "text-pink-600",
+    bgColor: "bg-pink-100",
+  },
+  {
+    label: "15 Pediatric Services",
+    number: "15",
+    icon: MdOutlineMedicalServices,
+    iconColor: "text-indigo-600",
+    bgColor: "bg-indigo-100",
+  },
+];
+
+export const aboutUs = {
+  title: "Top Choice For Children",
+  description:
+    "Our team of experienced and dedicated therapists is here to provide the guidance, support, and encouragement needed to overcome challenges and build skills. With our help, individuals can reach their goals and thrive.",
+  items: ["Great Staff", "Safety", "Experience", "Activities"],
+};
